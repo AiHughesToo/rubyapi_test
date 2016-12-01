f9f0baea76f765fc6934e9fbf072efd9db5c5686
AiHughesToo <alex.hughes@interapthq.com>
2016-12-01 13:29:35 -0500

    factoru girl

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
 
-  FactoryGirl.define do
-    factory :user do
-    end
-  end
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
 end
 
