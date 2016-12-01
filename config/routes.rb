231cb2685cc162ebf0f229414852e00d846d7605
AiHughesToo <alex.hughes@interapthq.com>
2016-12-01 16:06:38 -0500

    users end points

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
   devise_for :users
   # Api definition
-  namespace :api, defaults: {format: :json},
-                            constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json },
+            constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1,
-            constraints: ApiConstraints.new(version: 1, default: true) do
-    # We are going to list our resources here
+          constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+      resources :users, :only => [:show, :create]
     end
   end
 end
+
 
 
